151439c0b148233a556a516835b3bb1735518a75
akgit00 <tkfsecond@gmail.com>
2025-12-17 14:55:47 -0500

    create a class to display the user's profile

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,19 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/profile")
+@CrossOrigin
+public class ProfileController {
+
+    private UserDao userDao;
+    private ProfileDao profileDao;
--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -154,20 +154,4 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
             System.out.println("Error editing cart: " + e);
         }
     }
-
-    //this is a helper method to convert a database ResultSet row into a Product object
-    protected static Product mapRow(ResultSet row) throws SQLException {
-        int productId = row.getInt("product_id");
-        String name = row.getString("name");
-        BigDecimal price = row.getBigDecimal("price");
-        int categoryId = row.getInt("category_id");
-        String description = row.getString("description");
-        String subCategory = row.getString("subcategory");
-        int stock = row.getInt("stock");
-        boolean isFeatured = row.getBoolean("featured");
-        String imageUrl = row.getString("image_url");
-
-        //return a fully constructed Product
-        return new Product(productId, name, price, categoryId, description, subCategory, stock, isFeatured, imageUrl);
-    }
 }
