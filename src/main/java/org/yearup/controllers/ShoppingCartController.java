1a3316a164b09115abb04bb4f99d82bbb374c2d2
akgit00 <tkfsecond@gmail.com>
2025-12-18 12:56:19 -0500

    create a class represents the business logic for completing a checkout in an online store

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -139,6 +139,3 @@ public class ShoppingCartController
         return receipt;
     }
     }
-
-
-}
---
+++ src/main/java/org/yearup/models/CheckoutService.java
@@ -0,0 +1,27 @@
+package org.yearup.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yearup.data.OrderDao;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.ShoppingCartDao;
+
+import java.math.BigDecimal;
+
+@Service
+public class CheckoutService {
+
+    private ShoppingCartDao shoppingCartDao;
+    private OrderDao orderDao;
+    private ProductDao productDao;
+    private ProfileDao profileDao;
+
+    @Autowired
+    public CheckoutService(ShoppingCartDao shoppingCartDao, OrderDao orderDao, ProductDao productDao, ProfileDao profileDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.orderDao = orderDao;
+        this.productDao = productDao;
+        this.profileDao = profileDao;
+    }
